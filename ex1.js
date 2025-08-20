e1fa1944ef0de4c8d4befe3210d2896c725fd7ad
Aharon Shamsi <aharonshamsi@gmil.com>
2025-08-20 23:11:53 +0300

    finish lesson one

--- ex1.js
+++ ex1.js
@@ -8,4 +8,29 @@ Number = 10.5;
 Boolean = true
 var undefined
 var emptyValue = null; 
-cons
+
+// ========= operatoers ==========
+// השוואת משתנים
+var y = "7"
+var z = 7
+console.log(y == z); // This is on use
+console.log(y === z); // This is yes use
+
+if(y != z ) // This is no use
+{
+    console.log("y is not equal to z");
+}
+
+if(y !== z) // This is yes use
+{
+    console.log("y is not strictly equal to z");
+}  
+
+
+//======= window actions run in the console of googel chrom =========
+// alert()  Show a message to the user in a dialog box
+// prompt() Show a dialog box that prompts the user for input 
+// confirm() Show a dialog box with a message and two buttons: OK and Cancel
+
+//var userInput = prompt("Please enter your name:");   userInput = "Aharon" this is a string;
+//var userInput = +prompt("Please enter your name:");   userInput = "Aharon" this is a integer;
