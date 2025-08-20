5e6089bcbe18e74198ba55c64c9288298386751e
Aharon Shamsi <aharonshamsi@gmil.com>
2025-08-20 14:11:42 +0300

    first commit

---
+++ ex1.js
@@ -0,0 +1,11 @@
+
+// הצהרת משתנים
+var x = 30;
+var name = "Aharon"
+String = "Hello " + name + "! You are " + x + " years old."
+Number = 10;
+Number = 10.5;
+Boolean = true
+var undefined
+var emptyValue = null; 
+cons
